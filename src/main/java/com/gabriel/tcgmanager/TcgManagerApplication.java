b6f3de963620466d4ce4f2369693d6bc702b78d9
Gabriel Bernardes de Moura <gabrielbemoura@gmail.com>
2022-09-04 17:36:03 -0300

    first commit

---
+++ src/main/java/com/gabriel/tcgmanager/TcgManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.tcgmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcgManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcgManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabriel/tcgmanager/TcgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.tcgmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcgManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
